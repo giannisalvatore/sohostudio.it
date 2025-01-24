e59c285a0fb2af7c797112deb6c99d6d12c492f2
GianniSalvatore <giannisalvatore4@gmail.com>
2025-01-24 13:15:41 +0100

    Revert "x"
    This reverts commit c398efc1a287624f9a3e1f14a3d78024832a076a.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import vercelStatic from '@astrojs/vercel/static';
-
-export default defineConfig({
-    output: 'static',
-    adapter: vercelStatic()
-});
+// https://astro.build/config
+export default defineConfig({});

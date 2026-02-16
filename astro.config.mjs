eb57ded4f17068dc6ff44081850627cacb1c7b83
Gianni Salvatore <giannisalvatore4@gmail.com>
2026-02-16 21:23:22 +0100

    Fix SEO titles for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://www.sohostudio.it',
+    trailingSlash: 'never',
     integrations: [react(), tailwind(), sitemap(), icon()],
 });
